df82b28fa8cbbbade433d26639e9fe705339e877
Elson Khoza <contact@elsonkhoza.co.za>
2022-12-06 10:49:37 +0200

    Bottom navigation update

--- app/src/main/java/com/fienc/taxipassenger/MainActivity.java
+++ app/src/main/java/com/fienc/taxipassenger/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void iniViews() {
         bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setItemIconTintList(null);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
